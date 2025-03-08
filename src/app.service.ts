0e8c37035759d52ce550b8098e0a5f2f69129796
ennbook <ennbook@outlook.com>
2025-03-08 16:30:01 +0530

    swagger and doc

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
-
+/**
+ * This is the service for the app, initial test service.
+ */
 @Injectable()
 export class AppService {
+/**
+ * @example
+ * Hello World!
+ *
+ */
   getHello(): string {
-    return 'Hello World4!';
+    return 'Hello World!';
   }
 }
